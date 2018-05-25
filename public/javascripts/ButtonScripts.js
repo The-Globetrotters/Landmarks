faf198f4ac8019c7bdd6adb6f4bb661a0765b5d2
jeremyrkong <jeremyrkong@gmail.com>
2018-05-25 09:00:44 -0700

    Add files via upload

---
+++ public/javascripts/ButtonScripts.js
@@ -0,0 +1,7 @@
+document.getElementById("savedshow").addEventListener("click", function(){
+    document.getElementById("savedplaces").style.display = "block";
+}); 
+
+document.getElementById("savedclose").addEventListener("click", function(){
+    document.getElementById("savedplaces").style.display = "none";
+}); 
